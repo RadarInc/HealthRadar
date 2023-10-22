530fc17463e2ee71ffce789029ac9eca2ed00bcb
Adith Gangalakunta <adithgangalakunta@Adiths-MacBook-Air.local>
2023-10-22 10:55:44 -0500

    implement class based api calls

--- pages/api/auth.ts
+++ pages/api/auth.ts
@@ -1,33 +1,39 @@
-import axios, { AxiosInstance } from 'axios';
-import openai from 'openai';
+import OpenAI from "openai";
 
-const headers = {
-    "Content-Type": "application/json",
-    "Authorization": "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"
-};
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+});
 
-// Define the API endpoint
+var prompt: string = "";
+
+
+
+
 class HealthRadarContext {
-    private baseURL:string =  "https://api.openai.com/v1/chat/completions";
-    private axiosInstance: AxiosInstance = axios.create({ baseURL: this.baseURL });
+    constructor() {}
+    public async getSurveyQuestions(journalEntry: string): Promise<string> {
+        prompt = journalEntry;
+        const response = await openai.chat.completions.create({
+            model: "gpt-3.5-turbo",
+            messages: [
+                {
+                    "role": "system",
+                    "content": "You will be provided a journal entry from a construction worker, your task is to generate 10 survey questions that can be answered on a scale of 1-5"
+                },
+                {
+                    "role": "user",
+                    "content": prompt
+                }
+            ],
+            temperature: 0,
+            max_tokens: 256,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0,
+        });
+        const jsonQuestions = JSON.stringify(response.choices);
+        console.log(jsonQuestions);
 
-    constructor(){}
-
-    public async postHealthRadarData(body: any): Promise<any> {
-        
-        const response = await this.axiosInstance.post(this.baseURL, body, { headers });
-        try {
-        if (response.status === 200) {
-            return response.data;
-        } else {
-            throw new Error(`Error posting data to Health Radar: ${response.statusText}`);
-        }
-    } catch (error) {
-        throw new Error(`Error posting data to Health Radar: ${error}`);
-    }
-
-
+        return jsonQuestions;
     }
 }
-
-export default HealthRadarContext;
